13af31a1276b15797e5b10a0cadd23676375be74
windhamdavid <david@davidawindham.com>
2014-08-26 18:01:44 -0400

    config

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "drupal/"
+css_dir = "drupal/themes/*/css"
+sass_dir = "drupal/themes/*/sass"
+images_dir = "drupal/themes/*/img"
+javascripts_dir = "drupal/themes/*/js"
+
+environment = :development
+#environment = :production
+
+# enable relative paths to assets via compass helper functions.
+relative_assets = true
